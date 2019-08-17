23b94bec45b87cb037b7cc6e7c52ad03602f5478
lelledaniele <lelle.daniele@gmail.com>
2019-08-17 14:15:15 +0100

    Add stripe integration test for appcustomer

---
+++ customer/customerstripe_integration_test.go
@@ -0,0 +1,67 @@
+// +build stripe
+
+package appcustomer_test
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/customer"
+
+	appcurrency "github.com/lelledaniele/upaygo/currency"
+
+	appconfig "github.com/lelledaniele/upaygo/config"
+	appcustomer "github.com/lelledaniele/upaygo/customer"
+)
+
+func TestMain(m *testing.M) {
+	var fcp string
+
+	flag.StringVar(&fcp, "config", "", "Provide config file as an absolute path")
+	flag.Parse()
+
+	if fcp == "" {
+		fmt.Print("Integration Stripe test needs the config file absolute path as flag -config")
+		os.Exit(1)
+	}
+	fmt.Printf("provided path was %s\n", fcp)
+
+	fc, e := os.Open(fcp)
+	if e != nil {
+		fmt.Printf("Impossible to get configuration file: %v\n", e)
+		os.Exit(1)
+	}
+	defer fc.Close()
+
+	e = appconfig.ImportConfig(fc)
+	if e != nil {
+		fmt.Printf("Error durring file config import: %v", e)
+		os.Exit(1)
+	}
+
+	os.Exit(m.Run())
+}
+
+func TestNewStripe(t *testing.T) {
+	email := "email@email.com"
+	c, _ := appcurrency.New("EUR")
+
+	got, e := appcustomer.NewStripe(email, c)
+	if e != nil {
+		t.Errorf("error during the appcustomer creation with Stripe: %v", e)
+	}
+
+	if got.GetGatewayReference() == "" {
+		t.Errorf("The new customer.gateway_reference is empty, got: %v", got.GetGatewayReference())
+	}
+
+	if got.GetEmail() != email {
+		t.Errorf("The new customer.email is incorrect, got: %v want %v", got.GetEmail(), email)
+	}
+
+	params := &stripe.CustomerParams{}
+	_, _ = customer.Del(got.GetGatewayReference(), params)
+}
